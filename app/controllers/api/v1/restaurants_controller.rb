daf809fba3f754c4c04f78d6310d48a277dcec94
Konstantin Yatsenko <moahtdeep@gmail.com>
2021-12-16 21:28:28 +0900

    Added uodate endpoint

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,10 +1,25 @@
 class Api::V1::RestaurantsController < Api::V1::BaseController
+  before_action :set_restaurant, only: [ :show, :update ]
   def index
     @restaurants = policy_scope(Restaurant)
   end
 
   def show
+  end
+
+  def update
+    @restaurant.update(restaurant_params)
+    render :show
+  end
+
+  private
+
+  def set_restaurant
     @restaurant = Restaurant.find(params[:id])
     authorize @restaurant
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address)
   end
 end
