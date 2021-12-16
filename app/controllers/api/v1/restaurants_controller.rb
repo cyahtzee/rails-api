846c31411c2b310ac0f2b8ce53f5ea6c112ba4d9
Konstantin Yatsenko <moahtdeep@gmail.com>
2021-12-16 21:33:08 +0900

    Added validations to the models

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -8,8 +8,11 @@ class Api::V1::RestaurantsController < Api::V1::BaseController
   end
 
   def update
-    @restaurant.update(restaurant_params)
-    render :show
+    if @restaurant.update(restaurant_params)
+      render :show
+    else
+      render_error
+    end
   end
 
   private
@@ -21,5 +24,10 @@ class Api::V1::RestaurantsController < Api::V1::BaseController
 
   def restaurant_params
     params.require(:restaurant).permit(:name, :address)
+  end
+
+  def render_error
+  render json: { errors: @restaurant.errors.full_messages },
+    status: :unprocessable_entity
   end
 end
